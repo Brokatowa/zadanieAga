3c3e045ff8a35cd0941b8b498e84752bd7695526
AgNieszka <motyl09@onet.pl>
2022-08-27 15:27:30 +0200

    'agazadanieeeee'

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+    // alert("Hello! I am an alert box!!");
+
+
+    function myFunction() {
+        let name= ('Aga');
+
+        document.getElementById('aga').innerHTML = name;
+    }
+
+    myFunction ();
